2fc8bef6c54268e69ff62dc98b8d9b20d219997d
Damdidam <88735484+Damdidam@users.noreply.github.com>
2026-02-18 15:14:23 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -181,7 +181,9 @@ app.get('/validate', (req, res) => {
     </div>
   </body></html>`);
 });
-
+// privacy
+app.get('/privacy', (req, res) => res.sendFile(path.join(__dirname, '../frontend/privacy.html')));
+//
 // ═══════════════════════════════════════════════════════
 // START
 // ═══════════════════════════════════════════════════════
